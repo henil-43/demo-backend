d4e1c4ebcd4e5c27ed598ccf5a47b047c1339abd
agilepc115 <henil.r@agileinfoways.com>
2022-07-22 16:22:34 +0530

    added middleware in chats api

--- routes/chat.js
+++ routes/chat.js
@@ -1,9 +1,10 @@
 var express = require('express')
 const router = express.Router()
 const {addChat, getChats} = require('../controllers/chat')
+const { isAuthenticated } = require('../middlewares/middleware')
 
-router.get('/chats/:id', getChats);
+router.get('/chats/:id', isAuthenticated, getChats);
 
-router.post('/chat', addChat);
+router.post('/chat', isAuthenticated, addChat);
 
 module.exports = router
